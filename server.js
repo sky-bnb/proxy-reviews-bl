f98376fa5dd026cd67eed9aa848f35e1ada6039e
bjlagman12 <bjlagman@gmail.com>
2019-05-28 17:33:09 -0700

    Render components to proxy server

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 const app = express()
 const port = 3000
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-app.use(morgan('dev'));
 app.use(express.static('public'))
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(morgan('dev'));
+app.use(cors())
 
 app.get('/:propertyId', (req, res) => {
   res.sendfile(path.join(__dirname, '/public/index.html'));
